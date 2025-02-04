f779d6bd17cdfc33c2c3de5c555efc5a16dedabd
David Parseen <david.parseen@gmail.com>
2025-02-04 11:13:58 +0300

    feat: Enhance wallet connect button with suspense loading state

--- src/components/wallet-connect-button.tsx
+++ src/components/wallet-connect-button.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { Button } from "./ui/button";
+import { Suspense } from "react";
 import { WalletIcon } from "lucide-react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+
+const WalletMultiButton = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
 
 export function WalletConnectButton() {
   return (
     <div className="relative items-center gap-2 inline-flex bg-primary text-primary-foreground rounded-xl">
-      <WalletIcon  className="ml-2"/>
-      <WalletMultiButton />
+      <Suspense
+        fallback={
+          <Button className="font-semibold text-xl mx-4 my-2">
+            Loading...
+          </Button>
+        }
+      >
+        <WalletIcon className="ml-2" />
+        <WalletMultiButton />
+      </Suspense>
     </div>
   );
 }
